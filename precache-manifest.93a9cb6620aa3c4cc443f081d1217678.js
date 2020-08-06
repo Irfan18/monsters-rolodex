ffacc0798d6c349d133461a433ae9e56055dcdf7
irfan <irfan.shaikh@adapty.com>
2020-08-06 19:15:20 +0530

    Updates

---
+++ precache-manifest.93a9cb6620aa3c4cc443f081d1217678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef86df5abe727d9b8cbc7f4c24b480a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8d0b9209f753e24b446a",
+    "url": "/monsters-rolodex/static/css/main.da86d54d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d0b9209f753e24b446a",
+    "url": "/monsters-rolodex/static/js/main.a9ba5661.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
